38bb77459972b0041e7cee144ab85ae0564d0aec
Marc Dave <davevalencia17@gmail.com>
2024-02-13 21:41:43 +0800

    created a book filtering mini project Explored usage of v-for, v-if and computed properties Implemented a filtering feature to filter books set as favourite

--- app.js
+++ app.js
@@ -1,14 +1,61 @@
 const app = Vue.createApp({
   data() {
     return {
-      greeting: "Hello World!",
-      isActive: false,
+      // greeting: "Hello World!",
+      // count: 0,
+      // isActive: false,
+      isFiltered: false,
+      books: [
+        {
+          title: "Intro to Algorithms",
+          author: "Bob Smith",
+          year: 1997,
+          isFavourite: false,
+        },
+
+        {
+          title: "Database Fundamentals",
+          author: "John Doe",
+          year: 2001,
+          isFavourite: false,
+        },
+
+        {
+          title: "Creating WEB APIs",
+          author: "Les Jackson",
+          year: 2007,
+          isFavourite: true,
+        },
+      ],
     };
   },
 
   methods: {
     toggle() {
       this.isActive = !this.isActive;
+      this.increment();
+    },
+
+    increment() {
+      this.count += 1;
+    },
+
+    decrement() {
+      this.count -= 1;
+    },
+
+    setFavourite(book) {
+      book.isFavourite = !book.isFavourite;
+    },
+
+    showFave() {
+      this.isFiltered = !this.isFiltered;
+    },
+  },
+
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFavourite);
     },
   },
 
