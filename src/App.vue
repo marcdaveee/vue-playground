008f04e56baf1ea80d9f8352cbfab6a49a16f235
Marc Dave <davevalencia17@gmail.com>
2024-02-17 15:30:24 +0800

    Utilized Props Learned to use props to pass data from parent component to child component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Modal,
   },
 
   data() {
     return {
       title: "My First Vue App",
+      header: "Sign up for Giveaway!",
+      text: "Grap your coupons for discounts!",
     };
   },
 };
@@ -29,5 +34,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  margin: 10px 0;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+h1 {
+  color: darkblue;
+  margin: 20px 0;
+}
+
+.modal.sale {
+  background: darkgreen;
+  color: #fff;
+}
+
+.modal.sale h1 {
+  color: #fff;
+}
+</style>
